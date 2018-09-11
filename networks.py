c9a8a17ab8978e18a69f6bb1cc6417a939126cbf
sai <saikrishnagottipati@gmail.com>
2018-09-11 01:25:20 -0400

    added forward networks

---
+++ networks.py
@@ -0,0 +1,60 @@
+#import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class forward_in_h1(nn.Module):
+    def __init__(self):
+        super(forward_in_h1, self).__init__()
+        d_in = 784
+        num_h1 = 1024
+
+        self.linear1 = nn.Linear(d_in, num_h1)
+
+    def forward(self, x):
+        x = Variable(x.float())
+        x = F.relu(self.linear1(x))
+
+        return x
+
+class forward_h1_h2(nn.Module):
+    def __init__(self):
+        super(forward_h1_h2, self).__init__()
+        num_h1 = 1024
+        num_h2 = 512
+
+        self.linear2 = nn.Linear(num_h1, num_h2)
+
+    def forward(self, x):
+        x = Variable(x.float())
+        x = F.relu(self.linear2(x))
+
+        return x
+
+class forward_h2_h3(nn.Module):
+    def __init__(self):
+        super(forward_h2_h3, self).__init__()
+        num_h2 = 512
+        num_h3 = 100
+
+        self.linear3 = nn.Linear(num_h2, num_h3)
+
+    def forward(self, x):
+        x = Variable(x.float())
+        x = F.relu(self.linear3(x))
+
+        return x
+
+class forward_h3_dout(nn.Module):
+    def __init__(self):
+        super(forward_h3_dout, self).__init__()
+        num_h3 = 100
+        dout = 10
+
+        self.linear4 = nn.Linear(num_h3, dout)
+
+    def forward(self, x):
+        x = Variable(x.float())
+        x = F.log_softmax(x, dim = 1)
+
+        return x
