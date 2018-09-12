b1061e71d991787aaa8c56f5e02828cf4e15fe87
sai <saikrishnagottipati@gmail.com>
2018-09-11 21:45:17 -0400

    Added major computation functions

--- sdtp.py
+++ sdtp.py
@@ -51,6 +51,9 @@ class sdtp(object):
         self.h2_out = np.zeros((self.batch_size, self.num_h2))
         self.h3_out = np.zeros((self.batch_size, self.num_h3))
         self.dout_out = np.zeros((self.batch_size, self.dout))
+        self.h3_inverse = np.zeros((self.batch_size, self.num_h3))
+        self.h2_inverse = np.zeros((self.batch_size, self.num_h2))
+        self.h1_inverse = np.zeros((self.batch_size, self.num_h1))
 
         self.h1_out = Variable(torch.FloatTensor(self.h1_out))
         self.h1_out = self.h1_out.to(self.device)
@@ -64,14 +67,89 @@ class sdtp(object):
         self.dout_out = Variable(torch.FloatTensor(self.dout_out))
         self.dout_out = self.dout_out.to(self.device)
 
+        self.h3_inverse = Variable(torch.FloatTensor(self.h3_inverse))
+        self.h3_inverse = self.h3_inverse.to(self.device)
+
+        self.h2_inverse = Variable(torch.FloatTensor(self.h2_inverse))
+        self.h2_inverse = self.h2_inverse.to(self.device)
+
+        self.h1_inverse = Variable(torch.FloatTensor(self.h1_inverse))
+        self.h1_inverse = self.h1_inverse.to(self.device)
 
 
 
-    def forward_propagate():
+
+    def forward_propagate(self):
         self.h1_out = self.model_forward_in_h1(self.input_batch)
         self.h2_out = self.model_forward_h1_h2(self.h1_out)
         self.h3_out = self.model_forward_h2_h3(self.h2_out)
         self.dout_out = self.model_forward_h3_dout(self.h3_out)
+
+    def compute_inverses(self):
+        self.h3_inverse = self.h3_out - self.model_backward_dout_h3(self.dout_out) + self.model_backward_dout_h3(self.target_batch) # is this argmin same as tartget batch?
+        self.h2_inverse = self.h2_out - self.model_backward_h3_h2(self.h3_out) + self.model_backward_h3_h2(self.h3_inverse)
+        self.h1_inverse = self.h1_out - self.model_backward_h2_h1(self.h2_out) + self.model_backward_h2_h1(self.h2_inverse)
+
+    def train_inverses(self):
+        mu, sigma = 0, 0.1
+        m = torch.distributions.Normal(mu, sigma)
+
+        self.optimizer_dout_h3.train()
+        self.optimizer_dout_h3.zero_grad()
+        self.optimizer_h3_h2.train()
+        self.optimizer_h3_h2.zero_grad()
+        self.optimizer_h2_h1.train()
+        self.optimizer_h2_h1.zero_grad()
+
+        eps3 = m.sample((self.num_h3,))
+        h3_out_corrupt = self.h3_out + eps3
+        loss3 = self.L2loss(self.model_backward_dout_h3(self.model_forward_h3_dout(h3_out_corrupt)), self.h3_out)
+        loss3.backward()
+        self.optimizer_dout_h3.step()
+
+        eps2 = m.sample((self.num_h2,))
+        h2_out_corrupt = self.h2_out + eps2
+        loss2 = self.L2loss(self.model_backward_h3_h2(self.model_forward_h2_h3(h2_out_corrupt)), self.h2_out)
+        loss2.backward()
+        self.optimizer_h3_h2.step()
+
+        eps1 = m.sample((self.num_h1,))
+        h1_out_corrupt = self.h1_out + eps1
+        loss1 = self.L2loss(self.model_backward_h2_h1(self.model_forward_h1_h2(h1_out_corrupt)), self.h1_out)
+        loss1.backward()
+        self.optimizer_h2_h1.step()
+
+    def train_forward(self):
+        
+        self.optimizer_in_h1.train()
+        self.optimizer_h1_h2.train()
+        self.optimizer_h2_h3.train()
+        self.optimizer_h3_dout.train()
+
+        self.optimizer_in_h1.zero_grad()
+        self.optimizer_h1_h2.zero_grad()
+        self.optimizer_h2_h3.zero_grad()
+        self.optimizer_h3_dout.zero_grad()
+
+        forward_loss1 = self.L2loss(self.h1_out, self.h1_inverse) #just notation ambiguity. here h1_inverse is the inverse of h2. which is to be compared with h1 output. so, we are fine
+        forward_loss1.backward()
+        self.optimizer_in_h1.step()
+
+        forward_loss2 = self.L2loss(self.h2_out, self.h2_inverse)
+        forward_loss2.backward()
+        self.optimizer_h1_h2.step()
+
+        forward_loss3 = self.L2loss(self.h3_out, self.h3_inverse)
+        forward_loss3.backward()
+        self.optimizer_h2_h3.step()
+
+        forward_loss4 = self.L2loss(self.dout_out, self.target_batch)
+        forward_loss4.backward()
+        self.optimizer_h3_dout.step()
+
+
+
+
 
 
 
